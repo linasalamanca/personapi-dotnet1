076309144ad3c035b07f96679b9c88d1904cf374
linasalamanca <118871999+linasalamanca@users.noreply.github.com>
2024-10-15 21:23:06 -0500

    entidades creadas

---
+++ Dockerfile
@@ -0,0 +1,23 @@
+# Usa la imagen oficial de .NET 8 SDK para construir el proyecto
+FROM mcr.microsoft.com/dotnet/sdk:8.0 AS build-env
+WORKDIR /app
+
+# Copia el archivo .csproj y restaura las dependencias
+COPY personapi-dotnet/*.csproj ./personapi-dotnet/
+WORKDIR /app/personapi-dotnet
+RUN dotnet restore
+
+# Copia el resto del contenido del proyecto y publica la aplicación
+COPY . .
+RUN dotnet publish -c Release -o out
+
+# Usa una imagen ligera de .NET 8 para ejecutar la aplicación
+FROM mcr.microsoft.com/dotnet/aspnet:8.0
+WORKDIR /app
+COPY --from=build-env /app/personapi-dotnet/out .
+
+# Expone el puerto en el que la aplicación correrá
+EXPOSE 80
+
+# Define el comando que se ejecutará cuando el contenedor inicie
+ENTRYPOINT ["dotnet", "Script.sql"]
---
+++ personapi-dotnet1/Models/Entities/PersonaDbContext.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+
+namespace personapi_dotnet1.Models.Entities;
+
+public partial class PersonaDbContext : DbContext
+{
+    public PersonaDbContext()
+    {
+    }
+
+    public PersonaDbContext(DbContextOptions<PersonaDbContext> options)
+        : base(options)
+    {
+    }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see https://go.microsoft.com/fwlink/?LinkId=723263.
+        => optionsBuilder.UseSqlServer("Server=127.0.0.1;Database=persona_db;User Id=SA;Password=Your_password123;TrustServerCertificate=true");
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        OnModelCreatingPartial(modelBuilder);
+    }
+
+    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+}
